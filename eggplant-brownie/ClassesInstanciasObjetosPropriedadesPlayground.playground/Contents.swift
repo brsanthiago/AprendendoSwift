6f0985e1d0d67249acadbbb2550f29e8e59d4297
Bruno Santiago <bruno.oliveira@note1006.local>
2017-01-09 14:43:13 -0200

    Optionals com interrogação e exclamação + unwrapping com let

--- eggplant-brownie/ClassesInstanciasObjetosPropriedadesPlayground.playground/Contents.swift
+++ eggplant-brownie/ClassesInstanciasObjetosPropriedadesPlayground.playground/Contents.swift
@@ -12,3 +12,32 @@ print(brownie.name)
 var sundubu = Meal()
 sundubu.name = "Sundubu"
 print(sundubu.name)
+
+
+//OPTIONALS
+var name:String?
+name = "eggplant brownie"
+print(name)
+print(name!.uppercased())
+
+class MealOptional{
+    var happiness:Int?
+    var name:String?
+}
+
+
+var brownieOptional = MealOptional()
+
+
+print(brownieOptional.happiness)
+print(brownieOptional.name)
+
+brownieOptional.happiness = 3
+brownieOptional.name = "Optional Brownie"
+//print(brownieOptional.name?.uppercased())
+
+
+if let name = brownieOptional.name{
+    print("Opa, eu tenho um nome")
+    print(brownieOptional.name)
+}
