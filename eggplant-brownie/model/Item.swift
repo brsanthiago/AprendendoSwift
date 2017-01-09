cd41d0f09287c352155154b07d24911b1df83375
Bruno Santiago <bruno.oliveira@note1006.local>
2017-01-09 16:23:45 -0200

    Organizando o projeto com grupos

---
+++ eggplant-brownie/model/Item.swift
@@ -0,0 +1,19 @@
+//
+//  Item.swift
+//  eggplant-brownie
+//
+//  Created by Bruno Santiago on 09/01/17.
+//  Copyright © 2017 Bruno Santiago. All rights reserved.
+//
+
+import Foundation
+
+class Item{
+    let name:String;
+    let calories:Double;
+    
+    init(name:String, calories:Double) {
+        self.name = name;
+        self.calories = calories;
+    }
+}
